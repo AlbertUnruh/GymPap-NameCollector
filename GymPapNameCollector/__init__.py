aa12b101abb726e23534c26bac7dad028908b2aa
AlbertUnruh <AlbertUnruh@pm.me>
2023-08-24 18:13:06 +0200

    change `__init__.py` to don't import everything

--- GymPapNameCollector/__init__.py
+++ GymPapNameCollector/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "AlbertUnruh"
-__version__ = "0.0.2a"
+__version__ = "0.1.0a"
 __license__ = "GNU AGPLv3"
 __copyright__ = f"Copyright 2023-present (c) {__author__}"
 __url__ = "https://github.com/AlbertUnruh/GymPap-NameCollector"
@@ -10,15 +10,15 @@ from vendor.AlbertUnruhUtils.utils.logger import get_logger
 __root_logger__ = get_logger(None, level=0)
 
 
-from .analytics import *
-from .constants import *
-from .worldwidewifi import *
+from . import analytics
+from . import constants
+from . import worldwidewifi
 
 
 # crime section
 
-__USER_AGENT = USER_AGENT
-USER_AGENT = USER_AGENT.format(**globals())
+__USER_AGENT = constants.USER_AGENT
+constants.USER_AGENT = constants.USER_AGENT.format(**globals())
 # should be a *constant* by definition, but crimes have to be committed ^^
-__root_logger__.info(f"Set `USER_AGENT` from {__USER_AGENT!r} to {USER_AGENT!r}")
+__root_logger__.info(f"Set `USER_AGENT` from {__USER_AGENT!r} to {constants.USER_AGENT!r}")
 __root_logger__.debug(f"A copy of the original `USER_AGENT` has been set in `__USER_AGENT`")
--- GymPapNameCollector/__main__.py
+++ GymPapNameCollector/__main__.py
@@ -1,6 +1,6 @@
 import GymPapNameCollector
 
 
-browser = GymPapNameCollector.Browser()
+browser = GymPapNameCollector.worldwidewifi.Browser()
 for page in browser.iter_pages():
     print(page.find_article_urls())
